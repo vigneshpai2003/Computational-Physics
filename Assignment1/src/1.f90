8d919977d7355fe1df9ca0166c634ed9c78b7979
Vignesh Pai <vigneshpai2003@gmail.com>
2024-01-05 00:27:18 +0530

    using make

--- Assignment1/src/1.f90
+++ Assignment1/src/1.f90
@@ -1,35 +1,3 @@
-module utils
-    implicit none
-
-    private
-    public avg, write_array
-contains
-
-    function avg(arr) result(average)
-        real, intent(in) :: arr(:)
-        real :: average
-
-        average = sum(arr) / size(arr)
-    end function avg
-
-    subroutine write_array(target, format, arr)
-        integer, intent(in) :: target
-        character(*), intent(in) :: format
-        real, intent(in) :: arr(:)
-
-        integer :: i
-
-        do i = 1, size(arr)
-            if (format /= "") then
-                write(target, format) arr(i)
-            else
-                write(target, *) arr(i)
-            end if
-        end do
-    end subroutine
-
-end module utils
-
 program assignment
     use utils
     implicit none
