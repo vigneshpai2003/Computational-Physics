d511d7480c4f6faa4b567d675f53d19d7b853fd9
Vignesh Pai <vigneshpai2003@gmail.com>
2024-01-10 11:58:17 +0530

    bode

--- Assignment2/src/modules/integrate.f90
+++ Assignment2/src/modules/integrate.f90
@@ -65,8 +65,8 @@ contains
         real(8), optional :: dx(d)
         integer, optional :: N(d)
 
-        real(8) :: integral, dx_(d)
-        integer :: i, j, lattice(d), N_(d), weight
+        real(8) :: integral, dx_(d), weight
+        integer :: i, j, lattice(d), N_(d)
 
         if (present(dx)) then
             dx_ = dx
@@ -91,12 +91,12 @@ contains
                 end if
             end do
 
-            weight = 2.0d0 ** (count(mod(lattice, 2)==1) + d - count(mod(lattice, N_)==0))
+            weight = 2.0d0 ** (count(mod(lattice, 2)==1) - count(mod(lattice, N_)==0))
 
             integral = integral + f(a + lattice * dx_) * weight
         end do
 
-        integral = integral * product(dx_) / (3.0d0 ** d)
+        integral = integral * product(dx_) * ((2.0d0 / 3.0d0) ** d)
 
     end function
 
@@ -110,8 +110,8 @@ contains
         real(8), optional :: dx(d)
         integer, optional :: N(d)
 
-        real(8) :: integral, dx_(d)
-        integer :: i, j, lattice(d), N_(d), weight
+        real(8) :: integral, dx_(d), weight
+        integer :: i, j, lattice(d), N_(d)
 
         if (present(dx)) then
             dx_ = dx
@@ -136,12 +136,59 @@ contains
                 end if
             end do
 
-            weight = 1.50d0 ** (count(mod(lattice, 3)/=0)) * 2.0d0 ** (d - count(mod(lattice, N_)==0))
+            weight = 1.50d0 ** count(mod(lattice, 3)/=0) / 2.0d0 ** count(mod(lattice, N_)==0)
 
             integral = integral + f(a + lattice * dx_) * weight
         end do
 
-        integral = integral * product(dx_) * (0.3750d0 ** d)
+        integral = integral * product(dx_) * ((3.0d0 / 4.0d0) ** d)
+
+    end function
+
+    function bode(d, f, a, b, dx, N) result(integral)
+
+        integer, intent(in) :: d
+        procedure(integrable_function) :: f
+        real(8), intent(in) :: a(d), b(d)
+
+        ! exactly one of dx or N is expected to be given
+        real(8), optional :: dx(d)
+        integer, optional :: N(d)
+
+        real(8) :: integral, dx_(d), weight
+        integer :: i, j, lattice(d), N_(d)
+
+        if (present(dx)) then
+            dx_ = dx
+            N_ = nint((b - a) / dx_)
+        else if (present(N)) then
+            N_ = N
+            dx_ = (b - a) / N_
+        end if
+
+        integral = 0
+
+        lattice(:) = 0
+
+        do i = 1, product(N_ + 1)
+            ! go to next lattice point
+            do j = 1, d
+                lattice(j) = lattice(j) + 1
+                if (lattice(j) > N_(j)) then
+                    lattice(j) = mod(lattice(j), N_(j) + 1)
+                else
+                    exit
+                end if
+            end do
+
+            weight = (6.0d0 / 7.0d0) ** (count(mod(lattice, 4)==2)) &
+                    * (16.0d0 / 7.0d0) ** (count(mod(lattice, 2)==1)) &
+                    / 2.0d0 ** (count(mod(lattice, N_)==0))
+
+            integral = integral + f(a + lattice * dx_) * weight
+        end do
+
+        integral = integral * product(dx_) * ((28.0d0 / 45.0d0) ** d)
 
     end function
 
--- Assignment2/src/scratch.f90
+++ Assignment2/src/scratch.f90
@@ -2,7 +2,7 @@ function f(x) result(y)
     real(8), intent(in) :: x(:)
     real(8) :: y
 
-    y = x(1)**4
+    y = x(1)**5
 end function
 
 
@@ -13,7 +13,10 @@ program scratch
     procedure(integrable_function) :: f
     real(8) :: s
 
-    print *, simpson(1, f, [0.0d0], [1.0d0], N=[12])
-    print *, simpson2(1, f, [0.0d0], [1.0d0], N=[12])
+    s = bode(1, f, [0.0d0], [1.0d0], N=[4])
+
+    print *, simpson(1, f, [0.0d0], [1.0d0], N=[4])
+    print *, simpson2(1, f, [0.0d0], [1.0d0], N=[4])
+    print *, s
 
 end program scratch
