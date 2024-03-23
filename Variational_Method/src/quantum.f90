4c17e641b163153328c5b417656ff452acd87baa
Vignesh Pai <vigneshpai2003@gmail.com>
2024-03-24 03:42:07 +0530

    generated data

--- Variational_Method/src/quantum.f90
+++ Variational_Method/src/quantum.f90
@@ -1,5 +1,7 @@
 module quantum
     implicit none
+    
+    real(8), parameter :: pi = 2 * asin(1.0d0)
 
     ! Interface block for LAPACK subroutines
     interface
@@ -13,20 +15,81 @@ module quantum
 
 contains
 
-    subroutine H_matrix(H, n, a, b, V)
-        real(8), allocatable, intent(out) :: H(:, :)
-        integer, intent(in) :: n
+    subroutine H_matrix(H, a, b, V)
+        real(8), intent(out) :: H(:, :)
         real(8), intent(in) :: a, b, V
 
-        integer :: i, j
+        integer :: n, dim, i, j
+        real(8), allocatable :: k(:)
 
-        allocate(H(n, n))
+        dim = size(H, 1)
+        n = (dim - 1) / 2
 
-        do i = 1, n
-            do j = 1, n
-                H(i, j) = 1
+        allocate(k(dim))
+
+        do i = -n, n
+            k(n + i + 1) = 2 * pi * i / a
+        end do
+
+        do i = 1, dim
+            H(i, i) = k(i)**2/2 - V * b / a
+            do j = i + 1, dim
+                H(i, j) = - 2 * V / a * sin((k(j) - k(i)) * b / 2) / (k(j) - k(i))
             end do
         end do
+
+        deallocate(k)
     end subroutine
+
+    function find_lwork(jobz, uplo, n, a, lda, w) result(lwork)
+        character(1), intent(in) :: jobz, uplo
+        integer, intent(in) :: n, lda
+        real(8), intent(inout) :: a(lda,*), w(n)
+
+        integer :: lwork, info
+        real(8) :: work(1)
+
+        lwork = -1
+
+        call dsyev(jobz, uplo, n, a, lda, w, work, lwork, info)
+
+        lwork = nint(work(1))
+
+    end function
+
+    subroutine write_array(file, arr)
+        character(*), intent(in) :: file
+        real*8, intent(in) :: arr(:)
+
+        integer :: i, io
+
+        open(newunit=io, file=file)
+        do i = 1, size(arr, 1)
+            write(io, *) arr(i)
+        end do
+        close(io)
+    end subroutine
+
+    subroutine write_matrix(file, mat)
+        character(*), intent(in) :: file
+        real*8, intent(in) :: mat(:, :)
+
+        integer :: i, io
+
+        open(newunit=io, file=file)
+        do i = 1, size(mat, 1)
+            write(io, *) mat(i, :)
+        end do
+        close(io)
+    end subroutine
+
+    function str(k) result(string)
+        integer, intent(in) :: k
+        character(20) :: string
+
+        write(string, *) k
+
+        string = adjustl(string)
+    end function str
 
 end module quantum
--- Variational_Method/src/variational.f90
+++ Variational_Method/src/variational.f90
@@ -2,17 +2,68 @@ program scratch
     use quantum
     implicit none
 
-    integer :: n
+    integer :: n, dim, info, lwork, i
     real(8) :: a, b, V
-    real(8), allocatable :: H(:, :)
+    real(8), allocatable :: H(:, :), eigenvalues(:), work(:)
 
-    n = 10
-    a = 10.0d0
+    call execute_command_line("mkdir -p data/n/e data/n/v data/a/e data/a/v")
+
+    ! potential parameters
     b = 2.0d0
     V = 1.0d0
 
-    call H_matrix(H, n, a, b, V)
+    ! vary n
+    a = 5.0d0
+    do n = 10, 100, 5
 
-    deallocate(H)
+        dim = 2 * n + 1
+
+        allocate(H(dim, dim), eigenvalues(dim))
+
+        call H_matrix(H, a, b, V)
+
+        lwork = find_lwork('V', 'U', dim, H, dim, eigenvalues)
+        allocate(work(lwork))
+
+        call dsyev('V', 'U', dim, H, dim, eigenvalues, work, lwork, info)
+
+        if (info /= 0) then
+            print *, "Error Code: ", info
+            stop
+        end if
+
+        call write_array("data/n/e/"//trim(str(n))//".dat", eigenvalues)
+        call write_matrix("data/n/v/"//trim(str(n))//".dat", H)
+
+        deallocate(work, eigenvalues, H)
+    end do
+
+    ! vary a
+    n = 100
+    dim = 2 * n + 1
+    allocate(H(dim, dim), eigenvalues(dim))
+
+    do i = 5, 50, 5
+        a = i
+
+        call H_matrix(H, a, b, V)
+
+        lwork = find_lwork('V', 'U', dim, H, dim, eigenvalues)
+        allocate(work(lwork))
+    
+        call dsyev('V', 'U', dim, H, dim, eigenvalues, work, lwork, info)
+    
+        if (info /= 0) then
+            print *, "Error Code: ", info
+            stop
+        end if
+   
+        call write_array("data/a/e/"//trim(str(nint(a)))//".dat", eigenvalues)
+        call write_matrix("data/a/v/"//trim(str(nint(a)))//".dat", H)
+
+        deallocate(work)
+    end do
+
+    deallocate(eigenvalues, H)
 
 end program scratch
