8ecccc214747ad307bcbe3cb5edd66c74c516a12
Vignesh Pai <vigneshpai2003@gmail.com>
2024-04-06 23:09:47 +0530

    update

--- Molecular_Dynamics/src/md.f90
+++ Molecular_Dynamics/src/md.f90
@@ -1,7 +1,7 @@
 module md
     implicit none
 
-    real(8) :: L = 10, m = 1, r_c = 2.5, epsilon = 1, sigma = 1, kBT = 100
+    real(8) :: L, m, r_c, epsilon, sigma, kBT
 
 contains
 
@@ -17,7 +17,7 @@ contains
 
         F = 0
 
-        do i = 1, 3 * N, 3
+        do i = 1, 3 * N - 3, 3
             do j = i + 3, 3 * N, 3
 
                 dx = x(i : i + 2) - x(j : j + 2)
@@ -35,7 +35,45 @@ contains
             end do
         end do
     end subroutine
-    
+
+    subroutine update_x(N, x, v, F, dt)
+        integer, intent(in) :: N
+        real(8), intent(in) :: v(3 * N), F(3 * N), dt
+        real(8), intent(inout) :: x(3 * N)
+
+        x = x + v * dt + 0.5d0 * F * dt**2 / m
+        x = mod(x, L) + merge(L, 0.0d0, x < 0.0d0)
+    end subroutine
+
+    subroutine update_v(N, v, F, F_new, dt)
+        integer, intent(in) :: N
+        real(8), intent(in) :: F(3 * N), F_new(3 * N), dt
+        real(8), intent(inout) :: v(3 * N)
+        
+        v = v + 0.25d0 * (F + F_new) * dt / m
+    end subroutine
+
+    function avg_KE(N, v) result(E)
+        integer, intent(in) :: N
+        real(8), intent(in) :: v(3 * N)
+        real(8) :: E
+
+        integer :: i, j
+
+        do i = 1, 3 * N - 3, 3
+            do j = i + 1, N
+                E = 0.5d0 * m * sum(v * v) / N
+            end do
+        end do
+    end function
+
+    ! function avg_PE(N, x) result(E)
+    !     integer, intent(in) :: N
+    !     real(8), intent(in) :: x(3 * N)
+    !     real(8) :: E
+
+    !     E = 4 * epsilon * ((sigma / r)**12)
+    ! end function
 end module md
 
 program scratch
@@ -45,9 +83,15 @@ program scratch
     integer, parameter :: N = 10, N_t = 100
     real(8), parameter :: dt = 0.01d0
 
-    integer :: t
-    real(8) :: x(3 * N), v(3 * N), F(3 * N)
-    real(8) :: x_new(3 * N), v_new(3 * N), F_new(3 * N)
+    integer :: i
+    real(8) :: x(3 * N), v(3 * N), F(3 * N), F_new(3 * N)
+
+    L = 10
+    m = 1
+    r_c = 2.5d0
+    epsilon = 1
+    sigma = 1
+    kBT = 1
 
     call random_number(x)
     x = x * L
@@ -57,18 +101,14 @@ program scratch
     
     call force(N, x, F)
 
-    do t = 1, N_t
-        x_new = x + v * dt + 0.5d0 * F * dt**2 / m
+    do i = 1, N_t
+        call update_x(N, x, v, F, dt)
 
-        x_new = mod(x_new, L) + merge(L, 0.0d0, x_new < 0.0d0)
+        call force(N, x, F_new)
 
-        call force(N, x_new, F_new)
+        call update_v(N, v, F, F_new, dt)
 
-        v_new = v + 0.25d0 * (F + F_new) * dt / m
-
-        x = x_new
         F = F_new
-        v = v_new
 
         print *, x
     end do
