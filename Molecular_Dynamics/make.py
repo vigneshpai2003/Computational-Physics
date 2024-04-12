fb7d6449a87d7a5c6a61a8762dd51f0e43f326af
Vignesh Pai <vigneshpai2003@gmail.com>
2024-04-12 22:44:34 +0530

    separated module

--- Molecular_Dynamics/make.py
+++ Molecular_Dynamics/make.py
@@ -3,8 +3,11 @@ import sys; sys.path.append('../')
 from pymake import *
 from pymake import FortranCompiler as Compiler, FortranLinker as Linker
 
+md = Compiler('src/md.f90', 'default')
+md.add_flags('-O2', '-fopenmp')
+
 c = {
-    'md' : Compiler('src/md.f90', 'default'),
+    'test' : Compiler('src/test.f90', 'default', md),
 }
 
 l = dict((name, Linker(f'default:{name}', compiler)) for name, compiler in c.items())
@@ -16,8 +19,8 @@ for linker in [*l.values()]:
     linker.add_flags('-O2')
 
 commands = {
-    'all': ['md'],
-    'md': l['md'].binary,
+    'all': ['test'],
+    'test': l['test'].binary,
     'clean': lambda : (
         print('🔥 CLEANING'),
         sh(f'rm -rf build data'),
--- Molecular_Dynamics/src/md.f90
+++ Molecular_Dynamics/src/md.f90
@@ -108,7 +108,7 @@ contains
 
         real(8) :: E
 
-        integer :: i, j, k
+        integer :: i, j
         real(8) :: dx(3), r, F_c, V_c
 
         F_c = 12 * sigma**12 / r_c**13 - 6 * sigma**6 / r_c**7
@@ -175,79 +175,3 @@ contains
         string = adjustl(string)
     end function str
 end module md
-
-program scratch
-    use md
-    implicit none
-
-    integer, parameter :: N = 2197, N_L = nint(N**(1.0d0/3))
-    integer, parameter :: N_t = 1000, T_thermostat = 100, T_neighbors = 20
-    real(8), parameter :: dt = 0.005d0
-
-    integer :: i, j, k, p, n_neighbors(N)
-    real(8) :: x(3 * N), v(3 * N), F(3 * N), F_new(3 * N), KE, PE, R
-    logical :: neighbors(N, N)
-
-    call execute_command_line('mkdir -p data')
-
-    L = 20
-    m = 1
-    r_c = 2.5d0
-    epsilon = 1
-    sigma = 1
-    kBT = 1
-    R = 2 * 5 * sqrt(8 * kBT / (3 * m)) * T_neighbors * dt
-    R = 6
-
-    print *, "Number of Particles =", N
-    print *, "Size of Box =", L
-    print *, "Radius Cutoff =", r_c
-    print *, "Number of Iterations =", N_t
-    print *, "Time Step =", dt
-    print *, "Thermostat Every =", T_thermostat
-    print *, "Neighbors Calculation Every =", T_neighbors
-    print *, "Neighbor Radius =", r_c + R
-
-    p = 1
-    do i = 0, N_L
-        do j = 0, N_L
-            do k = 0, N_L
-                if (p .le. 3 * N) x(p: p + 2) = [i, j, k] * L / (N_L + 1)
-                p = p + 3
-            end do
-        end do
-    end do
-
-    call random_number(v)
-    v = sqrt(12 * kBT / m) * (v - 0.5d0)
-    
-    call calc_neighbors(N, x, neighbors, R)
-
-    call force(N, x, F, PE, neighbors)
-
-    do i = 1, N_t
-        call update_x(N, x, v, F, dt)
-
-        call force(N, x, F_new, PE, neighbors)
-
-        call update_v(N, v, F, F_new, dt)
-
-        F = F_new
-
-        if (mod(i, T_thermostat) == 0) then
-            call thermostat(N, v, kBT)
-            ! print *, "Thermostat"
-        end if
-
-        if (mod(i, T_neighbors) == 0) then
-            call calc_neighbors(N, x, neighbors, R)
-            ! n_neighbors = count(neighbors, 1)
-            ! print *, "Neighbors", sum(n_neighbors) / N, maxval(n_neighbors), minval(n_neighbors)
-        end if
-
-        KE = calc_KE(N, v)
-
-        print *, KE + PE, KE / N, PE / N, sum(v(1:3*N:3))
-    end do
-
-end program scratch
