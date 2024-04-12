7d7dedd9d2b55c61b15e5c2002b8eed0f903c1f6
Vignesh Pai <vigneshpai2003@gmail.com>
2024-04-12 19:49:49 +0530

    neighbor list

--- Molecular_Dynamics/src/md.f90
+++ Molecular_Dynamics/src/md.f90
@@ -4,43 +4,39 @@ module md
     real(8) :: L, m, r_c, epsilon, sigma, kBT
 
 contains
+    subroutine periodic_distance(dx, r)
+        real(8), intent(inout) :: dx(3)
+        real(8), intent(out) :: r
 
-    subroutine force(N, x, F, PE)
-        integer, intent(in) :: N
-        real(8), intent(in) :: x(3 * N)
-        real(8), intent(out) :: F(3 * N), PE
+        integer :: k
 
-        integer :: i, j, k
-        real(8) :: dx(3), r, F_r, F_c, V_c
-
-        F_c = 12 * sigma**12 / r_c**13 - 6 * sigma**6 / r_c**7
-        V_c = ((sigma / r_c)**12 - (sigma / r_c)**6) + F_c * r_c
-
-        F = 0
-        PE = 0
-
-        do i = 1, 3 * N - 3, 3
-            do j = i + 3, 3 * N, 3
-
-                dx = x(i : i + 2) - x(j : j + 2)
-                do k = 1, 3
-                    if (abs(dx(k)) > L / 2) dx(k) = dx(k) - sign(L, dx(k))
-                end do
-
-                r = norm2(dx)
-
-                if (r < r_c) then
-                    F_r = 12 * sigma**12 / r**13 - 6 * sigma**6 / r**7 - F_c
-                    F(i : i + 2) = F(i : i + 2) + F_r * dx / r
-                    F(j : j + 2) = F(j : j + 2) - F_r * dx / r
-
-                    PE = PE + (sigma / r)**12 - (sigma / r)**6 + F_c * r - V_c
-                end if
-            end do
+        do k = 1, 3
+            if (abs(dx(k)) > L / 2) dx(k) = dx(k) - sign(L, dx(k))
         end do
 
-        F = F * 4 * epsilon
-        PE = PE * 4 * epsilon
+        r = norm2(dx)
+    end subroutine
+
+    subroutine calc_neighbors(N, x, neighbors, R)
+        integer, intent(in) :: N
+        real(8), intent(in) :: x(3 * N)
+        logical, intent(out) :: neighbors(N, N)
+        real(8), intent(in) :: R
+
+        integer :: i, j
+        real(8) :: dx(3), d
+
+        neighbors = .false.
+
+        do i = 1, N - 1
+            do j = i + 1, N
+                dx = x(3 * i - 2 : 3 * i) - x(3 * j - 2 : 3 * j)
+                call periodic_distance(dx, d)
+
+                neighbors(i, j) = (d < r_c + R)
+                neighbors(j, i) = neighbors(i, j)
+            end do
+        end do
     end subroutine
 
     subroutine update_x(N, x, v, F, dt)
@@ -60,6 +56,43 @@ contains
         v = v + 0.5d0 * (F + F_new) * dt / m
     end subroutine
 
+    subroutine force(N, x, F, PE, neighbors)
+        integer, intent(in) :: N
+        real(8), intent(in) :: x(3 * N)
+        real(8), intent(out) :: F(3 * N), PE
+        logical, intent(in) :: neighbors(N, N)
+
+        integer :: i, j
+        real(8) :: dx(3), r, F_r, F_c, V_c
+
+        F_c = 12 * sigma**12 / r_c**13 - 6 * sigma**6 / r_c**7
+        V_c = ((sigma / r_c)**12 - (sigma / r_c)**6) + F_c * r_c
+
+        F = 0
+        PE = 0
+
+        do i = 1, 3 * N - 3, 3
+            do j = i + 3, 3 * N, 3
+                if (.not. neighbors((i + 2) / 3, (j + 2)/ 3)) cycle
+
+                dx = x(i : i + 2) - x(j : j + 2)
+
+                call periodic_distance(dx, r)
+
+                if (r < r_c) then
+                    F_r = 12 * sigma**12 / r**13 - 6 * sigma**6 / r**7 - F_c
+                    F(i : i + 2) = F(i : i + 2) + F_r * dx / r
+                    F(j : j + 2) = F(j : j + 2) - F_r * dx / r
+
+                    PE = PE + (sigma / r)**12 - (sigma / r)**6 + F_c * r - V_c
+                end if
+            end do
+        end do
+
+        F = F * 4 * epsilon
+        PE = PE * 4 * epsilon
+    end subroutine
+
     function calc_KE(N, v) result(E)
         integer, intent(in) :: N
         real(8), intent(in) :: v(3 * N)
@@ -68,9 +101,11 @@ contains
         E = 0.5d0 * m * sum(v * v)
     end function
 
-    function calc_PE(N, x) result(E)
+    function calc_PE(N, x, neighbors) result(E)
         integer, intent(in) :: N
         real(8), intent(in) :: x(3 * N)
+        logical, intent(in) :: neighbors(N, N)
+
         real(8) :: E
 
         integer :: i, j, k
@@ -83,13 +118,11 @@ contains
 
         do i = 1, 3 * N - 3, 3
             do j = i + 3, 3 * N, 3
+                if (.not. neighbors((i + 2) / 3, (j + 2)/ 3)) cycle
 
                 dx = x(i : i + 2) - x(j : j + 2)
-                do k = 1, 3
-                    if (abs(dx(k)) > L / 2) dx(k) = dx(k) - sign(L, dx(k))
-                end do
 
-                r = norm2(dx)
+                call periodic_distance(dx, r)
 
                 if (r < r_c) then
                     E = E + (sigma / r)**12 - (sigma / r)**6 + F_c * r - V_c
@@ -147,11 +180,13 @@ program scratch
     use md
     implicit none
 
-    integer, parameter :: N = 2197, N_t = 1000
+    integer, parameter :: N = 2197, N_L = nint(N**(1.0d0/3))
+    integer, parameter :: N_t = 1000, T_thermostat = 100, T_neighbors = 20
     real(8), parameter :: dt = 0.005d0
 
-    integer :: i, j, k, p, N_L
-    real(8) :: x(3 * N), v(3 * N), F(3 * N), F_new(3 * N), KE, PE
+    integer :: i, j, k, p, n_neighbors(N)
+    real(8) :: x(3 * N), v(3 * N), F(3 * N), F_new(3 * N), KE, PE, R
+    logical :: neighbors(N, N)
 
     call execute_command_line('mkdir -p data')
 
@@ -161,8 +196,17 @@ program scratch
     epsilon = 1
     sigma = 1
     kBT = 1
+    R = 2 * 5 * sqrt(8 * kBT / (3 * m)) * T_neighbors * dt
+    R = 6
 
-    N_L = nint(N**(1.0d0/3))
+    print *, "Number of Particles =", N
+    print *, "Size of Box =", L
+    print *, "Radius Cutoff =", r_c
+    print *, "Number of Iterations =", N_t
+    print *, "Time Step =", dt
+    print *, "Thermostat Every =", T_thermostat
+    print *, "Neighbors Calculation Every =", T_neighbors
+    print *, "Neighbor Radius =", r_c + R
 
     p = 1
     do i = 0, N_L
@@ -177,25 +221,33 @@ program scratch
     call random_number(v)
     v = sqrt(12 * kBT / m) * (v - 0.5d0)
     
-    call force(N, x, F, PE)
+    call calc_neighbors(N, x, neighbors, R)
+
+    call force(N, x, F, PE, neighbors)
 
     do i = 1, N_t
         call update_x(N, x, v, F, dt)
 
-        call force(N, x, F_new, PE)
+        call force(N, x, F_new, PE, neighbors)
 
         call update_v(N, v, F, F_new, dt)
 
         F = F_new
 
-        KE = calc_KE(N, v)
-
-        if (mod(i, 100) == 0) then
+        if (mod(i, T_thermostat) == 0) then
             call thermostat(N, v, kBT)
-            print *, "Thermostat"
+            ! print *, "Thermostat"
         end if
 
-        print *, KE + PE, KE / N, PE / N
+        if (mod(i, T_neighbors) == 0) then
+            call calc_neighbors(N, x, neighbors, R)
+            ! n_neighbors = count(neighbors, 1)
+            ! print *, "Neighbors", sum(n_neighbors) / N, maxval(n_neighbors), minval(n_neighbors)
+        end if
+
+        KE = calc_KE(N, v)
+
+        print *, KE + PE, KE / N, PE / N, sum(v(1:3*N:3))
     end do
 
 end program scratch
